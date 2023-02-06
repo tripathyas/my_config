ca4b282d2db67c83a70d92853645582c4bc8978b
ashutosh tripathy <tripathyas@vmware.com>
2023-02-06 19:16:10 +0530

    Fix install and configure script

--- configure.sh
+++ configure.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-mkdir -p ~/.config/nvim
+mkdir -p ~/.config
 mkdir -p ~/.vim/after/ftplugin/
 
-ln -sf ~/.config/my_config/.ackrc ~/.ackrc
-ln -sf ~/.config/my_config/.bashrc ~/.bashrc
-ln -sf ~/.config/my_config/.gitconfig ~/.gitconfig
-ln -sf ~/.config/my_config/.gitignore ~/.gitignore
-ln -sf ~/.config/my_config/.ignore ~/.ignore
-ln -sf ~/.config/my_config/.zshrc ~/.zshrc
-ln -sf ~/.config/my_config/.vimrc ~/.vimrc
+ln -sf ~/repo/private/my_config/.ackrc ~/.ackrc
+ln -sf ~/repo/private/my_config/.bashrc ~/.bashrc
+ln -sf ~/repo/private/my_config/.gitconfig ~/.gitconfig
+ln -sf ~/repo/private/my_config/.gitignore ~/.gitignore
+ln -sf ~/repo/private/my_config/.ignore ~/.ignore
+ln -sf ~/repo/private/my_config/.zshrc ~/.zshrc
+ln -sf ~/repo/private/my_config/.vimrc ~/.vimrc
+ln -sf ~/repo/private/my_config/.tmux.conf ~/.tmux.conf
+ln -sf ~/repo/private/my_config/.fzf.bash ~/.fzf.bash
+ln -sf ~/repo/private/my_config/.fzf.zsh ~/.fzf.zsh
+ln -sf ~/repo/private/my_config/.rgignore ~/.rgignore
+ln -sf ~/repo/private/my_config/nvim ~/.config/nvim
 # ln -sf ~/.config/my_config/init.vim ~/.config/nvim/init.vim
-ln -sf ~/.config/my_config/.tmux.conf ~/.tmux.conf
-ln -sf ~/.config/my_config/.fzf.bash ~/.fzf.bash
-ln -sf ~/.config/my_config/.fzf.zsh ~/.fzf.zsh
-ln -sf ~/.config/my_config/.rgignore ~/.rgignore
-ln -sf ~/.config/my_config/nvim ~/.config/nvim
 
-[ -f ~/.vim/autoload/plug.vim ] || echo "Install plug" $(curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim)
-vim -c 'PlugInstall' -c 'qa'
-nvim -c 'PlugInstall' -c 'qa'
+# [ -f ~/.vim/autoload/plug.vim ] || echo "Install plug" $(curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim)
+# vim -c 'PlugInstall' -c 'qa'
+# nvim -c 'PlugInstall' -c 'qa'
 
 
 # LOCAL SYSTEM SETUP
@@ -28,18 +28,18 @@ for file in ~/.config/my_config/filetype/*; do
   ln -sf $file ~/.vim/after/ftplugin/$(basename $file)
 done
 
-if [ "$(uname)" == "Darwin" ]; then
-  # Do something under Mac OS X platform
-  echo
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  # Do something under GNU/Linux platform
-  echo "Extracting nvim"
-  ./nvim.appimage --appimage-extract
-  sudo ln -sf ~/.config/my_config/squashfs-root/usr/bin/nvim /usr/bin/nvim
-fi
+# if [ "$(uname)" == "Darwin" ]; then
+#   # Do something under Mac OS X platform
+#   echo
+# elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+#   # Do something under GNU/Linux platform
+#   echo "Extracting nvim"
+#   ./nvim.appimage --appimage-extract
+#   sudo ln -sf ~/repo/private/my_config/squashfs-root/usr/bin/nvim /usr/bin/nvim
+# fi
 
 
-[[ -d ~/.tmux/plugins/tpm ]] || git clone --depth=1 --no-single-branch  https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# [[ -d ~/.tmux/plugins/tpm ]] || git clone --depth=1 --no-single-branch  https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # create cron job to update repo after reboot
-(crontab -l ; echo "@reboot sleep 10 && git -C ~/.config/my_config pull >/dev/null 2>&1") | sort - | uniq - | crontab -
+# (crontab -l ; echo "@reboot sleep 10 && git -C ~/repo/private/my_config pull >/dev/null 2>&1") | sort - | uniq - | crontab -
--- install.sh
+++ install.sh
@@ -8,27 +8,27 @@ cd ~/.config/my_config && sudo ./install.sh && ./configure.sh
 '
 #-------------------end_usage---------------------
 
-which apt
-if [ $? -eq 0 ]; then
-  mgr='apt-get'
-else
-  mgr='yum'
-fi
+# which apt
+# if [ $? -eq 0 ]; then
+#   mgr='apt-get'
+# else
+#   mgr='yum'
+# fi
+#
+# echo sudo $mgr install -y fzf ripgrep autojump 
+#
+# echo sudo $mgr update
+# echo sudo $mgr install -y fzf ripgrep python3-pip pylint tmux autojump ctags cscope tldr vim xmonad libghc-xmonad-contrib-dev dmenu
+# sudo $mgr update
+# sudo $mgr install -y git fzf ripgrep python3-pip pylint tmux autojump ctags cscope tldr vim xmonad libghc-xmonad-contrib-dev dmenu
 
-echo sudo $mgr install -y fzf ripgrep autojump 
 
-echo sudo $mgr update
-echo sudo $mgr install -y fzf ripgrep python3-pip pylint tmux autojump ctags cscope tldr vim xmonad libghc-xmonad-contrib-dev dmenu
-sudo $mgr update
-sudo $mgr install -y git fzf ripgrep python3-pip pylint tmux autojump ctags cscope tldr vim xmonad libghc-xmonad-contrib-dev dmenu
+# if [ "$(uname)" == "Darwin" ]; then
+#   # Do something under Mac OS X platform
+#   echo
+# elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+#   # Do something under GNU/Linux platform
+# fi
 
-
-if [ "$(uname)" == "Darwin" ]; then
-  # Do something under Mac OS X platform
-  echo
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  # Do something under GNU/Linux platform
-fi
-'
 sudo apt-get install -y git fzf ripgrep python3-pip pylint tmux autojump cscope tldr vim neovim exuberant-ctags curl
-'
+
