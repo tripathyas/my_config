115156ce762b8ad91b600da1a5bc07645e60b06b
Ashutosh Tripathy <ashutosh_tripathy@outlook.com>
2024-03-23 21:48:34 +0530

    update nvim config

--- nvim/lua/myconfig.lua
+++ nvim/lua/myconfig.lua
@@ -15,6 +15,12 @@ require("formatter").setup({
     json = {
       require("formatter.filetypes.json").prettier,
     },
+    xml = {
+      require("formatter.filetypes.json").prettier,
+    },
+    vdx = {
+      require("formatter.filetypes.json").prettier,
+    },
     ["*"] = {
       require("formatter.filetypes.any").remove_trailing_whitespace,
     },
