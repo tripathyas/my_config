79be3e84475937aabcf24209268c6921ddbc830a
Ashutosh Tripathy <ashutosh_tripathy@outlook.com>
2024-10-07 21:02:02 +0530

    add new files as part of kickstart plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -907,6 +907,7 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+  { 'tpope/vim-fugitive' },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
