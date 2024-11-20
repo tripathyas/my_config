faa99c114a8bafeaef477ef0d8f15b12b97994ea
Ashutosh Tripathy <ashtripa@ashtripa-mn7641.linkedin.biz>
2024-11-20 14:12:34 +0530

    update configure and install script

--- configure.sh
+++ configure.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-mkdir -p ~/.config
-mkdir -p ~/.vim/after/ftplugin/
-
-ln -sf ~/my_config/.ackrc ~/.ackrc
-ln -sf ~/my_config/.bashrc ~/.bashrc
-ln -sf ~/my_config/.gitconfig ~/.gitconfig
-ln -sf ~/my_config/.gitignore ~/.gitignore
-ln -sf ~/my_config/.ignore ~/.ignore
-ln -sf ~/my_config/.zshrc ~/.zshrc
-ln -sf ~/my_config/.tmux.conf ~/.tmux.conf
-ln -sf ~/my_config/.fzf.bash ~/.fzf.bash
-ln -sf ~/my_config/.fzf.zsh ~/.fzf.zsh
-ln -sf ~/my_config/.rgignore ~/.rgignore
-ln -sf ~/my_config/nvim ~/.config/nvim
+# mkdir -p ~/.config
+# mkdir -p ~/.vim/after/ftplugin/
+# 
+# ln -sf ~/my_config/.ackrc ~/.ackrc
+# ln -sf ~/my_config/.bashrc ~/.bashrc
+# ln -sf ~/my_config/.gitconfig ~/.gitconfig
+# ln -sf ~/my_config/.gitignore ~/.gitignore
+# ln -sf ~/my_config/.ignore ~/.ignore
+# ln -sf ~/my_config/.zshrc ~/.zshrc
+# ln -sf ~/my_config/.tmux.conf ~/.tmux.conf
+# ln -sf ~/my_config/.fzf.bash ~/.fzf.bash
+# ln -sf ~/my_config/.fzf.zsh ~/.fzf.zsh
+# ln -sf ~/my_config/.rgignore ~/.rgignore
+# ln -sf ~/my_config/nvim ~/.config/nvim
 # ln -sf ~/.config/my_config/init.vim ~/.config/nvim/init.vim
 # ln -sf ~/repo/private/my_config/.vimrc ~/.vimrc
+ln -sf ~/repo/personal/my_config/nvim/init.lua ~/.config/nvim/init.lua
+ln -sf ~/repo/personal/my_config/nvim ~/.config/
 
 # [ -f ~/.vim/autoload/plug.vim ] || echo "Install plug" $(curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim)
 # vim -c 'PlugInstall' -c 'qa'
